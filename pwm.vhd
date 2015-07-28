a3fff5c3f35e6c07131e63a96f5d432d8da40ef3
amin <m.amin.kashi@gmail.com>
2015-07-28 13:08:02 +0430

    help+clean up

--- pwm.vhd
+++ pwm.vhd
@@ -10,34 +10,40 @@ entity pwm is
 			clk: in std_logic);
 end pwm;
 
+--help:
+--resolution of ocr is 2^10 (if n=11) ,counters counts till 2^10 
+--if counter is smaller than ocr then the output pin becaomes 1
+--else it becomes 0
+--            ______________         ______________         ______________ 
+--    _______|   <- ocr ->  |_______|   <- ocr ->  |_______|   <- ocr ->  |  
+    
+--    <-       2^10       -> <-       2^10       -> <-       2^10       ->   
+
 architecture Behavioral of pwm is
 	signal Cnt, Cnt_1, Cnt_2: std_logic_vector(n-1 downto 0):=(others=>'0');
-	--signal oc_ins : std_logic_vector(n-1 downto 0);
 begin
-  -- oc_ins (7 downto 1) <= oc_in (6 downto 0);
-   --oc_ins (0) <= '0';	
+
 	
 	process(Clk)
    begin
 	
 		if rising_edge(Clk) then
---		cnt_clk <= cnt_clk + 1;
---		
---	if(cnt_clk="00001011") then
---	 cnt_clk <="00000000";
 			oc_out <='1';
 			if(Cnt >= oc_in) then
 				oc_out <='0';
 			end if;
 		end if;
---		end if;
+		
 	end process;
+	
+	
 	process(Clk)
    begin
 		if rising_edge(Clk) then
 			Cnt_1 <= Cnt_1+1;
 		end if;
 	end process;
+	
 	process(Clk)
    begin
 		if falling_edge(Clk) then
@@ -45,15 +51,6 @@ begin
 		end if;
 	end process;
 	
-	Cnt <= Cnt_1;-- + Cnt_2;-- 
-------------------------     cnt<=cnt+'1';
-------------------------    if  (cnt = "00001011") then
-------------------------	     cnt <= "00000000";
-------------------------		  Cnt_1 <= Cnt_1+1;
-------------------------	     oc_out <='1';
-------------------------		  if(Cnt_1 >= oc_in) then
-------------------------		   oc_out <='0';
-------------------------			 end if;
-------------------------		    end if;
-------------------------          end process;		
+	Cnt <= Cnt_1;
+	
           end Behavioral;
